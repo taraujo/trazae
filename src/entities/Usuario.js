c193e655ae793a4c7d8e9e7271c1e36a52481db7
Thasso Araújo <thassoaraaujo@gmail.com>
2020-06-11 20:21:45 -0300

    Recriando o Backend em Laravel

--- src/entities/Usuario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UsuarioSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-});
-
-module.exports = mongoose.model('Usuario', UsuarioSchema);
